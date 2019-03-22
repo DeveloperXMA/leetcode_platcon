f43a0b8e7f3466ffd0df03c0b0599eaaf64ca0a0
SomeguyMa <fullstackguy@gmail.com>
2019-03-22 00:13:58 -0700

    add tslint, utility files

--- __tests__/pascal_triangle.test.ts
+++ __tests__/pascal_triangle.test.ts
@@ -1,5 +1,9 @@
 import getRow from "../src/Recursion/Pascal_triangle";
 
-test("basic", () => {
+test("1 should return [1, 1]", () => {
   expect(getRow(1)).toEqual([1, 1]);
+});
+
+test("3 should return [1, 3, 3, 1]", () => {
+  expect(getRow(3)).toEqual([1, 3, 3, 1]);
 });
