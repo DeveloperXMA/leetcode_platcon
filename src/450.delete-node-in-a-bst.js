c36e193086a99491b740d88dc84beabddd7380c3
SomeguyMa <fullstackguy@gmail.com>
2019-04-01 21:44:13 -0700

    add number of islands, delete a node in BST

---
+++ src/450.delete-node-in-a-bst.js
@@ -0,0 +1,98 @@
+/*
+ * @lc app=leetcode id=450 lang=javascript
+ *
+ * [450] Delete Node in a BST
+ *
+ * https://leetcode.com/problems/delete-node-in-a-bst/description/
+ *
+ * algorithms
+ * Medium (39.49%)
+ * Total Accepted:    60.3K
+ * Total Submissions: 152.7K
+ * Testcase Example:  '[5,3,6,2,4,null,7]\n3'
+ *
+ * Given a root node reference of a BST and a key, delete the node with the
+ * given key in the BST. Return the root node reference (possibly updated) of
+ * the BST.
+ * 
+ * Basically, the deletion can be divided into two stages:
+ * 
+ * Search for a node to remove.
+ * If the node is found, delete the node.
+ * 
+ * 
+ * 
+ * Note: Time complexity should be O(height of tree).
+ * 
+ * Example:
+ * 
+ * root = [5,3,6,2,4,null,7]
+ * key = 3
+ * 
+ * ⁠   5
+ * ⁠  / \
+ * ⁠ 3   6
+ * ⁠/ \   \
+ * 2   4   7
+ * 
+ * Given key to delete is 3. So we find the node with value 3 and delete it.
+ * 
+ * One valid answer is [5,4,6,2,null,null,7], shown in the following BST.
+ * 
+ * ⁠   5
+ * ⁠  / \
+ * ⁠ 4   6
+ * ⁠/     \
+ * 2       7
+ * 
+ * Another valid answer is [5,2,6,null,4,null,7].
+ * 
+ * ⁠   5
+ * ⁠  / \
+ * ⁠ 2   6
+ * ⁠  \   \
+ * ⁠   4   7
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} key
+ * @return {TreeNode}
+ */
+
+
+var deleteNode = function(root, key) {
+  if (root === null) {
+    return null;
+  }
+  if (root.val > key) {
+    root.left = deleteNode(root.left, key);
+  } else if (root.val < key) {
+    root.right = deleteNode(root.right, key);
+  } else if (root.val === key) {
+    if (!root.left && !root.right) {
+      root = null;
+    } else if (!root.left) {
+      root = root.right;
+    } else if (!root.right) {
+      root = root.left;
+    } else if (root.left && root.right) {
+      let curr = root.right;
+      while(curr && curr.left) {
+        curr = curr.left;
+      }
+      root.val = curr.val;
+      root.right = deleteNode(root.right, curr.val);
+    }
+  }
+  return root;
+};
+
