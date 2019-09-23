21692a1857cd78edb74409a368605f38883e05cd
Sandman-Ren <sandman_ren@qq.com>
2019-09-22 21:15:42 -0400

    Simplified code

--- Swap Node In Pairs/solution1.java
+++ Swap Node In Pairs/solution1.java
@@ -1,6 +1,3 @@
-/* 
-    A partially recursive solution to the iterative algorithm.
- */
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -10,48 +7,9 @@
  * }
  */
 class Solution {
-    
-    private void swap(ListNode predecessor, ListNode left, ListNode right, ListNode successor) {
-        predecessor.next = right;
-        right.next = left;
-        left.next = successor;
-    }
-    
-    /**
-        Section:
-            previous (nullable) -> left -> right -> next (nullable)
-            
-            next = right.next
-            previous.next = right;
-            right.next = left
-            left.next = next;
-    */
-    
     public ListNode swapPairs(ListNode head) {
-        if (head == null) {return head;}
-        if (head.next == null) {return head;}
-        // swap the first pair
-        ListNode predecessor = null;
-        ListNode left = head;
-        ListNode right = left.next;
-        ListNode successor = right.next;
-
-        // swap: predecessor.next = right, right.next = left, left.next =successor
-        right.next = left;
-        left.next = successor;
-
-        // record the final result
-        ListNode result = right;
-
-        // update
-        predecessor = left;
-        predecessor.next = recur(predecessor.next);
-        return result;
+        return recur(head);
     }
-
-    /* 
-        A recursive implementation of the iterative algorithm
-     */
      private ListNode recur(ListNode head) {
          if (head == null) {return null;}
          if (head.next == null) {return head;}
