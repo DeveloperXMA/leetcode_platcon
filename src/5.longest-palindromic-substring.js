d4db33f45f7d69da2ed3b0e82c79bd98c9282099
Mr.Ma <fullstackguy@gmail.com>
2020-02-27 18:53:49 -0800

    5 longest palindromic substring

--- src/5.longest-palindromic-substring.js
+++ src/5.longest-palindromic-substring.js
@@ -8,6 +8,20 @@
  * @return {string}
  */
 var longestPalindrome = function(s) {
-    
+    let dp = [...Array(s.length)].map(() => new Array(s.length).fill(false));
+    let answer = "";
+    let maxLength = 0;
+    for (let j = 0; j < s.length;j++) {
+      for (let i = 0; i <= j; i++) {
+        dp[i][j] = (s[i] === s[j] && ( j - i <= 2 || dp[i+1][j-1]));
+        if (dp[i][j]) {
+          if (j - i + 1 > maxLength) {
+            maxLength = j - i + 1;
+            answer = s.slice(i, j + 1);
+          }
+        }
+      }
+    }
+    return answer;
 };
 
