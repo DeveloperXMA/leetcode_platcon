b21d29314217c1cc4120c77bc2113fd7ed02a7ea
SomeguyMa <fullstackguy@gmail.com>
2020-03-23 21:07:53 -0700

    211 add and search word data structure design

--- src/interview.js
+++ src/interview.js
@@ -1,32 +1,65 @@
-var fn = function(x) {
-  var z = 4;
-  return function(y) {
-    return x + y + z;
+var TreeNode = function (val) {
+  this.val = val;
+  this.map = new Map();
+  this.isComplete = false;
+}
+
+/**
+ * Initialize your data structure here.
+ */
+var WordDictionary = function () {
+  this.root = new TreeNode(null);
+};
+
+/**
+ * Adds a word into the data structure. 
+ * @param {string} word
+ * @return {void}
+ */
+WordDictionary.prototype.addWord = function (word) {
+  let curr = this.root;
+  for (let i = 0; i < word.length; i++) {
+    if (curr.map.has(word[i])) {
+      curr = curr.map.get(word[i]);
+    } else {
+      curr.map.set(word[i], new TreeNode(word[i]));
+      curr = curr.map.get(word[i]);
+    }
+    if (i + 1 === word.length) {
+      curr.isComplete = true;
+    }
   }
-}
+};
 
-result = fn(5)(7); // return 5 + 7 + 4
-
-var fn2 = function(x) {
-  var y = 4;
-  return function(y) {
-    return x + y;
+/**
+ * Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. 
+ * @param {string} word
+ * @return {boolean}
+ */
+WordDictionary.prototype.search = function (word) {
+  var helper = function (node, word) {
+    if (word.length === 0) return node.isComplete;
+    if (word[0] === '.') {
+      let children = node.map.values();
+      let res = false;
+      for (let child of children) {
+        res = res || helper(child, word.slice(1));
+      }
+      return res;
+    } else {
+      if (node.map.has(word[0])) {
+        return helper(node.map.get(word[0]), word.slice(1));
+      } else {
+        return false;
+      }
+    }
   }
-}
+  return helper(this.root, word);
+};
 
-result2 = fn2(5)(7); // retrun 5 + 7
-
-var Parent = function() {};
-Parent.prototype.myFunction = function() {
-  alert("parent");
-}
-
-var Child = function() {};
-Child.prototype.myFunction = function() {
-  alert('Child');
-}
-Child.prototype = new Parent();
-
-var child = new Child();
-child.myFunction();
-console.log("123")
+let dic = new WordDictionary();
+dic.addWord("at");
+dic.addWord("and");
+dic.addWord("an");
+dic.addWord("add");
+dic.search("a");
