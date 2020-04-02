c80896498debf91c91387341dace0c4e36865e42
Xinrui Ma <fullstackguy@gmail.com>
2020-04-01 20:18:09 -0700

    695. Max Area of Island

---
+++ src/DFS/695.max-area-of-island.js
@@ -0,0 +1,84 @@
+/*
+ * @lc app=leetcode id=695 lang=javascript
+ *
+ * [695] Max Area of Island
+ *
+ * https://leetcode.com/problems/max-area-of-island/description/
+ *
+ * algorithms
+ * Medium (60.50%)
+ * Likes:    1574
+ * Dislikes: 72
+ * Total Accepted:    130.5K
+ * Total Submissions: 214.4K
+ * Testcase Example:  '[[1,1,0,0,0],[1,1,0,0,0],[0,0,0,1,1],[0,0,0,1,1]]'
+ *
+ * Given a non-empty 2D array grid of 0's and 1's, an island is a group of 1's
+ * (representing land) connected 4-directionally (horizontal or vertical.) You
+ * may assume all four edges of the grid are surrounded by water.
+ * 
+ * Find the maximum area of an island in the given 2D array. (If there is no
+ * island, the maximum area is 0.)
+ * 
+ * Example 1:
+ * 
+ * 
+ * [[0,0,1,0,0,0,0,1,0,0,0,0,0],
+ * ⁠[0,0,0,0,0,0,0,1,1,1,0,0,0],
+ * ⁠[0,1,1,0,1,0,0,0,0,0,0,0,0],
+ * ⁠[0,1,0,0,1,1,0,0,1,0,1,0,0],
+ * ⁠[0,1,0,0,1,1,0,0,1,1,1,0,0],
+ * ⁠[0,0,0,0,0,0,0,0,0,0,1,0,0],
+ * ⁠[0,0,0,0,0,0,0,1,1,1,0,0,0],
+ * ⁠[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+ * 
+ * Given the above grid, return 6. Note the answer is not 11, because the
+ * island must be connected 4-directionally.
+ * 
+ * Example 2:
+ * 
+ * 
+ * [[0,0,0,0,0,0,0,0]]
+ * Given the above grid, return 0.
+ * 
+ * Note: The length of each dimension in the given grid does not exceed 50.
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var maxAreaOfIsland = function(grid) {
+    if (grid === null || grid.length === 0) {
+      return 0;
+    }
+    let ans = 0;
+    for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid[0].length; j++) {
+        if (grid[i][j] === 1) {
+          ans = Math.max(ans, dfs(grid, i, j));
+        }
+      }
+    }
+    return ans;
+};
+
+const dfs = (grid, row, col) => {
+
+  if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] === 0) {
+    return 0;
+  }
+
+  grid[row][col] = 0;
+
+  return (1 + 
+    dfs(grid, row - 1, col) + 
+    dfs(grid, row + 1, col) + 
+    dfs(grid, row, col - 1) + 
+    dfs(grid, row, col + 1)
+  )
+}
+// @lc code=end
+
