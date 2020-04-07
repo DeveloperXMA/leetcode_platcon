79e033c12e8720db8b4806f47b736a4d1e1647f6
Xinrui Ma <fullstackguy@gmail.com>
2020-04-06 22:32:50 -0700

    1021 remove outermost parentheses

---
+++ src/1021.remove-outermost-parentheses.js
@@ -0,0 +1,118 @@
+/*
+ * @lc app=leetcode id=1021 lang=javascript
+ *
+ * [1021] Remove Outermost Parentheses
+ *
+ * https://leetcode.com/problems/remove-outermost-parentheses/description/
+ *
+ * algorithms
+ * Easy (76.57%)
+ * Likes:    358
+ * Dislikes: 499
+ * Total Accepted:    73.6K
+ * Total Submissions: 95.8K
+ * Testcase Example:  '"(()())(())"'
+ *
+ * A valid parentheses string is either empty (""), "(" + A + ")", or A + B,
+ * where A and B are valid parentheses strings, and + represents string
+ * concatenation.  For example, "", "()", "(())()", and "(()(()))" are all
+ * valid parentheses strings.
+ * 
+ * A valid parentheses string S is primitive if it is nonempty, and there does
+ * not exist a way to split it into S = A+B, with A and B nonempty valid
+ * parentheses strings.
+ * 
+ * Given a valid parentheses string S, consider its primitive decomposition: S
+ * = P_1 + P_2 + ... + P_k, where P_i are primitive valid parentheses strings.
+ * 
+ * Return S after removing the outermost parentheses of every primitive string
+ * in the primitive decomposition of S.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "(()())(())"
+ * Output: "()()()"
+ * Explanation: 
+ * The input string is "(()())(())", with primitive decomposition "(()())" +
+ * "(())".
+ * After removing outer parentheses of each part, this is "()()" + "()" =
+ * "()()()".
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "(()())(())(()(()))"
+ * Output: "()()()()(())"
+ * Explanation: 
+ * The input string is "(()())(())(()(()))", with primitive decomposition
+ * "(()())" + "(())" + "(()(()))".
+ * After removing outer parentheses of each part, this is "()()" + "()" +
+ * "()(())" = "()()()()(())".
+ * 
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: "()()"
+ * Output: ""
+ * Explanation: 
+ * The input string is "()()", with primitive decomposition "()" + "()".
+ * After removing outer parentheses of each part, this is "" + "" = "".
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * S.length <= 10000
+ * S[i] is "(" or ")"
+ * S is a valid parentheses string
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {string} S
+ * @return {string}
+ */
+var removeOuterParentheses = function(S) {
+    let stack = [];
+    for (let ch of S) {
+      stack.push(ch);
+    }
+    let res = "";
+    let count = 0;
+    let curr = "";
+    while (stack.length) {
+      curr = stack.pop();
+      if (curr === ')') {
+        if (count > 0) {
+          res = curr + res;
+        }
+        count++;
+      } else {
+        if (count > 1) {
+          res = curr + res;
+        }
+        count--;
+      }
+    }
+    return res;
+};
+// @lc code=end
+
