a8f870d331148ba79b2690dc10dce827553ca4f7
Xinrui Ma <fullstackguy@gmail.com>
2020-04-07 19:45:37 -0700

    706 design hash map

---
+++ src/Design/706.design-hash-map.js
@@ -0,0 +1,127 @@
+/*
+ * @lc app=leetcode id=706 lang=javascript
+ *
+ * [706] Design HashMap
+ *
+ * https://leetcode.com/problems/design-hashmap/description/
+ *
+ * algorithms
+ * Easy (58.93%)
+ * Likes:    677
+ * Dislikes: 90
+ * Total Accepted:    75.8K
+ * Total Submissions: 127.3K
+ * Testcase Example:  '["MyHashMap","put","put","get","get","put","get", "remove", "get"]\n[[],[1,1],[2,2],[1],[3],[2,1],[2],[2],[2]]'
+ *
+ * Design a HashMap without using any built-in hash table libraries.
+ * 
+ * To be specific, your design should include these functions:
+ * 
+ * 
+ * put(key, value) : Insert a (key, value) pair into the HashMap. If the value
+ * already exists in the HashMap, update the value.
+ * get(key): Returns the value to which the specified key is mapped, or -1 if
+ * this map contains no mapping for the key.
+ * remove(key) : Remove the mapping for the value key if this map contains the
+ * mapping for the key.
+ * 
+ * 
+ * 
+ * Example:
+ * 
+ * 
+ * MyHashMap hashMap = new MyHashMap();
+ * hashMap.put(1, 1);          
+ * hashMap.put(2, 2);         
+ * hashMap.get(1);            // returns 1
+ * hashMap.get(3);            // returns -1 (not found)
+ * hashMap.put(2, 1);          // update the existing value
+ * hashMap.get(2);            // returns 1 
+ * hashMap.remove(2);          // remove the mapping for 2
+ * hashMap.get(2);            // returns -1 (not found) 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * All keys and values will be in the range of [0, 1000000].
+ * The number of operations will be in the range of [1, 10000].
+ * Please do not use the built-in HashMap library.
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Initialize your data structure here.
+ */
+ var MyHashMap = function() {
+  this.mod = 2741; // This is a large prime number to avoi collision
+  this.position = [...Array(this.mod)].map(() => ([]));
+};
+
+/**
+* value will always be non-negative. 
+* @param {number} key 
+* @param {number} value
+* @return {void}
+*/
+MyHashMap.prototype.put = function(key, value) {
+  let index = key % this.mod;
+  let array = this.position[index];
+  let i = 0;
+  for (; i < array.length; i++) {
+      if (array[i].key === key) {
+          array[i].value = value
+          break;
+      }
+  }
+  if (i === array.length) {
+      array.push({
+          key,value
+      })
+  }
+};
+
+/**
+* Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key 
+* @param {number} key
+* @return {number}
+*/
+MyHashMap.prototype.get = function(key) {
+  let index = key % this.mod;
+  let array = this.position[index];
+  let i = 0;
+  for (; i < array.length; i++) {
+      if (array[i].key === key) {
+          return array[i].value;
+      }
+  }
+  if (i === array.length) return -1;
+};
+
+/**
+* Removes the mapping of the specified value key if this map contains a mapping for the key 
+* @param {number} key
+* @return {void}
+*/
+MyHashMap.prototype.remove = function(key) {
+  let index = key % this.mod;
+  let array = this.position[index];
+  for (let i = 0; i < array.length; i++) {
+      if (array[i].key === key) {
+          array.splice(i, 1);
+          break;
+      }
+  }
+};
+
+/** 
+* Your MyHashMap object will be instantiated and called as such:
+* var obj = new MyHashMap()
+* obj.put(key,value)
+* var param_2 = obj.get(key)
+* obj.remove(key)
+*/
+
